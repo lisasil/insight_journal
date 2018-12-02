8b93852d46ff98d9db518a01eefde5af66db52cc
Lisa <le63@purdue.edu>
2018-12-01 22:58:09 -0500

    added css

--- insight_journal/urls.py
+++ insight_journal/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.entry_list, name='entry_list'),
+    path('entry/<int:pk>/', views.entry, name='entry'),
 ]
--- insight_journal/views.py
+++ insight_journal/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Entry
 
 def entry_list(request):
-    return render(request, 'insight_journal/entry_list.html', {})
+    entries = Entry.objects.order_by('-title')
+    return render(request, 'insight_journal/entry_list.html', {'entries': entries})
+
+def entry(request, pk):
+    entry = get_object_or_404(Entry, pk=pk)
+    return render(request, 'insight_journal/entry.html', {'entry': entry})
