91bc01a666b23cdadca9abd83c9025ed262f4b99
Lisa <le63@purdue.edu>
2018-12-02 00:25:19 -0500

    added navigation and more CSS

--- insight_journal/urls.py
+++ insight_journal/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.entry_list, name='entry_list'),
     path('entry/<int:pk>/', views.entry, name='entry'),
+    path('entry/new', views.new, name='new'),
+    path('entry/<int:pk>/edit/', views.edit, name='edit'),
 ]
--- insight_journal/views.py
+++ insight_journal/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Entry
+from .forms import EntryForm
+from django.shortcuts import redirect
 
 def entry_list(request):
     entries = Entry.objects.order_by('-title')
@@ -8,3 +10,31 @@ def entry_list(request):
 def entry(request, pk):
     entry = get_object_or_404(Entry, pk=pk)
     return render(request, 'insight_journal/entry.html', {'entry': entry})
+
+def new(request):
+    if request.method == "POST":
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            entry = form.save(commit=False)
+            entry.author = request.user
+            entry.save()
+            return redirect('entry', pk=entry.pk)
+
+    else:
+        form = EntryForm()
+
+    return render(request, 'insight_journal/new_entry.html', {'form': form})
+
+def edit(request, pk):
+    entry = get_object_or_404(Entry, pk=pk)
+    if request.method == "POST":
+        form = EntryForm(request.POST, instance=entry)
+        if form.is_valid():
+            entry = form.save(commit=False)
+            entry.author = request.user
+            entry.save()
+            return redirect('entry', pk=entry.pk)
+    else:
+        form = EntryForm(instance=entry)
+
+    return render(request, 'insight_journal/edit_entry.html', {'form': form})
